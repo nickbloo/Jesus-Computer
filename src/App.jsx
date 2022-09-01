94fe5a372b2bdec6ab971c94ba0d9c4c031b7bfe
Calvin See <calvinfrancissee@gmail.com>
2022-08-31 17:24:16 -0700

    Created api request for bible verse

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Verse from "./components/Verse.jsx";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { api_key } from '../config.js';
 
 const VERSES = [
   `JER.29.11`,
@@ -27,33 +28,46 @@ const VERSES = [
 ];
 //yarn add axios
 
+const config = {
+  headers: {
+    "api-key": api_key
+  }
+}
+
 export default function App() {
   const [verse, setVerse] = useState("other verse");
-  const [verseId, setVerseId] = useState("JER.29.11");
+  const [verseId, setVerseId] = useState("1CO.1.1");
   //Get a database of bible verses
+
+function randomChapter (x, y) {
+  return math.Random()
+}
 
   useEffect(() => {
     console.log("Stream Bladee");
+    // getNewVerse();
   }, []);
-
-  const bibleId = "de4e12af7f28f599-01"
 
   function getNewVerse () {
     console.log("Get a new verse");
-    axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/search?query=${verseId}`, () => {
-      
-    })
+    axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/verses/${verseId}?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false&use-org-id=false`, config)
+      .then((res) => {
+        console.log(res.data.data);
+        const newVerse = res.data.data.content;
+        setVerse(newVerse);
+      })
+      .catch((err) => {
+        //better error catching
+        console.error(err);
+      });
   }
 
   return (
     <div className="App">
       <Verse verse={verse} />
       <div>
-      <button onClick={getNewVerse}>
-        Get a new verse
-      </button>
+        <button onClick={getNewVerse}>Get a new verse</button>
       </div>
-
     </div>
   )
 }
---
+++ src/bibleData.js
@@ -0,0 +1,18 @@
+const books = [
+  "GEN",
+  "EXO",
+  "LEV",
+  "NUM",
+  "DEU",
+  "JOS",
+  "JUD",
+  "RUT",
+  "1SA",
+  "2SA",
+  "1KI",
+  "2KI",
+  "1CH",
+  "2CH",
+  "EZR",
+  ""
+]
