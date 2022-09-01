4a125869d0b566d36265540d5e3897cf54661048
Calvin See <calvinfrancissee@gmail.com>
2022-08-31 18:53:23 -0700

    removed console log

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,6 @@ import { api_key } from '../config.js';
 import { books, chapters } from './bibleData.js';
 import { getCookie, setCookie } from "./cookieHelpers.js";
 
-const VERSES = [
-  `JER.29.11`,
-  `PSA.23`,
-  `1COR.4.4-8`,
-  `PHP.4.13`,
-  `JHN.3.16`,
-  `ROM.8.28`,
-  `ISA.41.10`,
-  `PSA.46.1`,
-  `GAL.5.22-23`,
-  `HEB.11.1`,
-  `2TI.1.7`,
-  `1COR.10.13`,
-  `PRO.22.6`,
-  `ISA.40.31`,
-  `JOS.1.9`,
-  `HEB.12.2`,
-  `MAT.11.28`,
-  `ROM.10.9-10`,
-  `PHP.2.3-4`,
-  `MAT.5.43-44`,
-];
-//yarn add axios
-
 const config = {
   headers: {
     "api-key": api_key
@@ -76,8 +52,6 @@ export default function App() {
   }, [verse]);
 
   function getVerseList () {
-    console.log("Get a new verse");
-    // `https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/verses/${verseId}?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false&use-org-id=false`
     const num = Math.floor(Math.random() * books.length);
     const book = books[num];
     const chapter = randomNum(chapters[num]);
@@ -106,9 +80,6 @@ export default function App() {
   return (
     <div className="App">
       <Verse verse={verse} />
-      <div>
-        <button onClick={getVerseList}>Get a new verse</button>
-      </div>
     </div>
   )
 }
