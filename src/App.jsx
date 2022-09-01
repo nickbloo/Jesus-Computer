ebf459b8b3808a12d9a678ad96ae86d2f3f3c6fe
Calvin See <calvinfrancissee@gmail.com>
2022-08-31 18:22:37 -0700

    Gets a new random verse on reload

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Verse from "./components/Verse.jsx";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { api_key } from '../config.js';
+import { books, chapters } from './bibleData.js';
 
 const VERSES = [
   `JER.29.11`,
@@ -34,39 +35,60 @@ const config = {
   }
 }
 
+function randomNum (x) {
+  return Math.floor(Math.random() * (x) + 1);
+}
+
 export default function App() {
   const [verse, setVerse] = useState("other verse");
-  const [verseId, setVerseId] = useState("1CO.1.1");
+  const [verseId, setVerseId] = useState(null);
+  // const [loaded, setLoaded] = useState(false);
   //Get a database of bible verses
-
-function randomChapter (x, y) {
-  return math.Random()
-}
 
   useEffect(() => {
     console.log("Stream Bladee");
-    // getNewVerse();
+    getVerseList();
   }, []);
 
-  function getNewVerse () {
+  useEffect(() => {
+    if (verseId) {
+      getVerse();
+    }
+  }, [verseId]);
+
+  function getVerseList () {
     console.log("Get a new verse");
-    axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/verses/${verseId}?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false&use-org-id=false`, config)
+    // `https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/verses/${verseId}?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false&use-org-id=false`
+    const num = Math.floor(Math.random() * books.length);
+    const book = books[num];
+    const chapter = randomNum(chapters[num]);
+    axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/chapters/${book}.${chapter}/verses`, config)
       .then((res) => {
-        console.log(res.data.data);
-        const newVerse = res.data.data.content;
-        setVerse(newVerse);
+        const verses = res.data.data;
+        const verseNum = randomNum(verses.length);
+        setVerseId(verses[verseNum].id);
       })
       .catch((err) => {
-        //better error catching
+        //TODO: better error catching
         console.error(err);
+        setTimeout(getVerseList, 10000);
       });
+  }
+
+  function getVerse () {
+    axios.get(`https://api.scripture.api.bible/v1/bibles/de4e12af7f28f599-01/verses/${verseId}?content-type=text&include-notes=false&include-titles=false&include-chapter-numbers=false&include-verse-numbers=false&include-verse-spans=false&use-org-id=false`, config)
+      .then((res) => {
+        const verse = res.data.data.content;
+        setVerse(verse);
+      })
+      .catch((err) => console.error(err));
   }
 
   return (
     <div className="App">
       <Verse verse={verse} />
       <div>
-        <button onClick={getNewVerse}>Get a new verse</button>
+        <button onClick={getVerseList}>Get a new verse</button>
       </div>
     </div>
   )
