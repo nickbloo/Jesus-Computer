d8af3d868b6657fedc443823a4f494ba9482ca07
nickbloo <nickbloo2@gmail.com>
2022-09-14 19:32:11 -0500

    gh-pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState, useEffect } from "react";
 import useDaily from "./components/useDaily.jsx";
 import Scroll from "./components/Scroll.jsx";
 import Navbar from "./components/Navbar.jsx";
+import background from "./assets/background.png";
+import cloudbackground from "./assets/cloudbackground.png";
+import hearts from "./assets/hearts.png";
+import jesusscroll from "./assets/jesusscroll.png";
+import scroll from "./assets/scroll.png";
 
 export default function App() {
   const [scroll, setScroll] = useState("VERSE");
